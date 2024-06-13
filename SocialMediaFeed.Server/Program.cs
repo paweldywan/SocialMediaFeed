3c10e52760a80d4388373764df1e4af16a79bfba
Paweł Dywan <paweldywan@outlook.com>
2024-06-13 16:51:18 +0200

    Clean

--- SocialMediaFeed.Server/Program.cs
+++ SocialMediaFeed.Server/Program.cs
@@ -7,29 +7,30 @@ namespace SocialMediaFeed.Server
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            var services = builder.Services;
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            services.AddControllers();
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen();
 
             var app = builder.Build();
 
             app.UseDefaultFiles();
+
             app.UseStaticFiles();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
+
                 app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
